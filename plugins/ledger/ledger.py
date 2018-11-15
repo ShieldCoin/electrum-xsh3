272fb6d1d4f703812deab387074d2ecf3ad9953b
zakurai <zakurai@shieldx.sh>
2018-11-15 12:01:07 +1100

    Fix Ledger incompatibility

--- plugins/ledger/ledger.py
+++ plugins/ledger/ledger.py
@@ -442,7 +442,7 @@ class Ledger_KeyStore(Hardware_KeyStore):
             self.get_client().enableAlternate2fa(False)
             if segwitTransaction:
                 self.get_client().startUntrustedTransaction(True, inputIndex,
-                                                            chipInputs, redeemScripts[inputIndex])
+                                                            chipInputs, redeemScripts[inputIndex], 0x04)
                 if changePath:
                     # we don't set meaningful outputAddress, amount and fees
                     # as we only care about the alternateEncoding==True branch
@@ -462,7 +462,7 @@ class Ledger_KeyStore(Hardware_KeyStore):
                 while inputIndex < len(inputs):
                     singleInput = [ chipInputs[inputIndex] ]
                     self.get_client().startUntrustedTransaction(False, 0,
-                                                            singleInput, redeemScripts[inputIndex])
+                                                            singleInput, redeemScripts[inputIndex], 0x04)
                     inputSignature = self.get_client().untrustedHashSign(inputsPaths[inputIndex], pin, lockTime=tx.locktime)
                     inputSignature[0] = 0x30 # force for 1.4.9+
                     signatures.append(inputSignature)
@@ -470,7 +470,7 @@ class Ledger_KeyStore(Hardware_KeyStore):
             else:
                 while inputIndex < len(inputs):
                     self.get_client().startUntrustedTransaction(firstTransaction, inputIndex,
-                                                            chipInputs, redeemScripts[inputIndex])
+                                                            chipInputs, redeemScripts[inputIndex], 0x04)
                     if changePath:
                         # we don't set meaningful outputAddress, amount and fees
                         # as we only care about the alternateEncoding==True branch
